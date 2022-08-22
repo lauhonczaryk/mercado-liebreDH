8a6f1dd091c882d909c19283cd277df258457946
lauhonczaryk <lchonczaryk@gmail.com>
2022-08-22 20:17:15 -0300

    commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () =>{
     console.log('Servidor corriendo');
